158fe09b3d38cece8d5f17bb08153b5bab2ff344
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-07-20 20:01:56 +0800

    Update mod.rs

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -18,8 +18,15 @@ impl Database {
             .await?
             .interact(move |conn| {
                 let sql = "create table if not exists users (
-                    id integer primary key,
-                    name text not null unique
+                    id text not null primary key,
+                    name text not null unique,
+                    preferred_username text not null,
+                    inbox text not null,
+                    outbox text not null,
+                    local boolean not null,
+                    public_key text not null,
+                    private_key text,
+                    last_refreshed_at datetime not null
                 )";
 
                 conn.execute(sql, ())
