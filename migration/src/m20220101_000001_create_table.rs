ac49576341a4fe0a912ebbf5664012bd1448fa30
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-07-22 12:11:51 +0800

    user table

--- migration/src/m20220101_000001_create_table.rs
+++ migration/src/m20220101_000001_create_table.rs
@@ -25,7 +25,32 @@ impl MigrationTrait for Migration {
                     .col(ColumnDef::new(Post::Text).string().not_null())
                     .to_owned(),
             )
-            .await
+            .await?;
+
+        manager
+            .create_table(
+                Table::create()
+                    .table(User::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(User::Id)
+                            .string()
+                            .not_null()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(User::Name).string().not_null().unique_key())
+                    .col(ColumnDef::new(User::PreferredUsername).string().not_null())
+                    .col(ColumnDef::new(User::Inbox).string().not_null())
+                    .col(ColumnDef::new(User::Outbox).string().not_null())
+                    .col(ColumnDef::new(User::Local).boolean().not_null())
+                    .col(ColumnDef::new(User::PublicKey).string().not_null())
+                    .col(ColumnDef::new(User::PrivateKey).string().not_null())
+                    .col(ColumnDef::new(User::LastRefreshedAt).date_time().not_null())
+                    .to_owned(),
+            )
+            .await?;
+
+        Ok(())
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
@@ -34,7 +59,13 @@ impl MigrationTrait for Migration {
 
         manager
             .drop_table(Table::drop().table(Post::Table).to_owned())
-            .await
+            .await?;
+
+        manager
+            .drop_table(Table::drop().table(User::Table).to_owned())
+            .await?;
+
+        Ok(())
     }
 }
 
@@ -45,4 +76,18 @@ enum Post {
     Id,
     Title,
     Text,
+}
+
+#[derive(Iden)]
+enum User {
+    Table,
+    Id,
+    Name,
+    PreferredUsername,
+    Inbox,
+    Outbox,
+    Local,
+    PublicKey,
+    PrivateKey,
+    LastRefreshedAt,
 }
