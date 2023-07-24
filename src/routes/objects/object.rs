33fb68208f3659eaec7e5c16e8347fdd474e2a67
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-07-24 23:52:20 +0800

    return note object

--- src/routes/objects/object.rs
+++ src/routes/objects/object.rs
@@ -1,39 +1,37 @@
 use activitypub_federation::{
     config::Data,
-    // traits::Object
+    traits::Object
 };
 use axum::{
-    // Json,
+    Json,
     debug_handler,
     extract::Path,
-//   response::IntoResponse,
-//   http::StatusCode
 };
-// use sea_orm::*;
+use sea_orm::*;
 
 use crate::{
   AppData,
-//   entities::{
-//     prelude::*,
-//     post::Model as DbPost
-//   },
-  error::Error
+  entities::{
+    prelude::*,
+    post::Model as DbPost
+  },
+  error::Error,
+  objects::post::Note
 };
 
 #[debug_handler]
 pub async fn object(
   Path(object): Path<String>,
-  _data: Data<AppData>,
-) -> Result<(), Error> {
+  data: Data<AppData>,
+) -> Result<Json<Note>, Error> {
     tracing::info!("Reading object {}", object);
-    // let object_id = format!("https://{}/o/{}", data.domain(), object);
-    // let db_post: DbPost = Post::find_by_id(object_id)
-    //     .one(&data.conn)
-    //     .await?
-    //     .unwrap();
 
-    // TODO: db_post.into_json()
-    Ok(())
+    let object_id = format!("https://{}/o/{}", data.domain(), object);
+    let db_post: DbPost = Post::find_by_id(object_id)
+        .one(&data.conn)
+        .await?
+        .unwrap();
+    let json_post: Note = db_post.into_json(&data).await?;
 
-    // (StatusCode::OK, db_post.into_json(data))
+    Ok(Json(json_post))
 }
