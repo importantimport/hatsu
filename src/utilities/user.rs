304ae7e4ce4ab03689eed2a00bd4eb672c2d00f7
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-07-27 23:34:28 +0800

    fix

--- src/utilities/user.rs
+++ src/utilities/user.rs
@@ -1,28 +1,28 @@
 use scraper::{Html, Selector};
 use serde::{Deserialize, Serialize};
-use url::Url;
+// use url::Url;
 
 use crate::{
     error::Error,
-    utilities::absolutize_relative_url,
+    // utilities::absolutize_relative_url,
 };
 
 /// User Site Feed
 /// TODO: Option<String> => Option<Url>
 #[derive(Deserialize, Serialize)]
 pub struct Feed {
-    json: Option<String>,
-    atom: Option<String>,
-    rss: Option<String>,
+    pub json: Option<String>,
+    // pub atom: Option<String>,
+    // pub rss: Option<String>,
 }
 
 /// 从网站获取 Feed 链接
 pub async fn get_site_feed(domain: String) -> Result<Feed, Error> {
-    let response = reqwest::get(&domain).await?;
+    let response = reqwest::get(format!("https://{}", &domain)).await?;
     let text = response.text().await?;
     let document = Html::parse_document(&text);
 
-    fn feed_auto_discovery(document: &Html, domain: &str, kind: &str) -> Result<String, Error> {
+    fn feed_auto_discovery(document: &Html, domain: &str, kind: &str) -> Result<Option<String>, Error> {
         let selector = Selector::parse(&format!("link[rel=\"alternate\"][type=\"{}\"]", kind)).unwrap();
         let link = document.select(&selector)
             .next()
@@ -31,15 +31,16 @@ pub async fn get_site_feed(domain: String) -> Result<Feed, Error> {
             .attr("href")
             .unwrap()
             .to_string();
-        let absolute_link = absolutize_relative_url(Url::parse(&link)?, domain.to_string())?.to_string();
+        // let absolute_link = absolutize_relative_url(Url::parse(&link)?, domain.to_string())?.to_string();
 
-        Ok(absolute_link)
+        // Ok(absolute_link)
+        Ok(Some(link))
     }
 
     let feed = Feed {
-        json: Some(feed_auto_discovery(&document, &domain, "application/feed+json")?),
-        atom: Some(feed_auto_discovery(&document, &domain,"application/atom+xml")?),
-        rss: Some(feed_auto_discovery(&document, &domain, "application/rss+xml")?),
+        json: feed_auto_discovery(&document, &domain, "application/feed+json")?,
+        // atom: feed_auto_discovery(&document, &domain,"application/atom+xml")?,
+        // rss: feed_auto_discovery(&document, &domain, "application/rss+xml")?,
     };
 
     Ok(feed)
