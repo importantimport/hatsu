f9df5a11d7ae3c411a2f68a4bd8b6b65514bd2ae
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-08-01 23:53:37 +0800

    Update outbox.rs

--- src/protocol/collections/outbox.rs
+++ src/protocol/collections/outbox.rs
@@ -10,11 +10,13 @@ pub struct Outbox {
   #[serde(rename = "type")]
   kind: OrderedCollectionType,
   id: Url,
+  // ordered_items: WithContext<>
   ordered_items: Vec<()>,
   total_items: u64
 }
 
 /// TODO: items (contents)
+/// 应该只输出当前 Feed 存在的文章，其余视为过期
 impl Outbox {
   pub fn new(outbox_id: Url) -> Result<Self, AppError> {
     Ok(Self {
