a2e5582de91a98eaa68e9a0b47eb965bc767e174
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-08-02 11:51:13 +0800

    fix migration

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,12 +1,16 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20220101_000001_create_table;
+mod m20230802_000001_user_follower;
 
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![Box::new(m20220101_000001_create_table::Migration)]
+        vec![
+            Box::new(m20220101_000001_create_table::Migration),
+            Box::new(m20230802_000001_user_follower::Migration)
+        ]
     }
 }
---
+++ migration/src/m20230802_000001_user_follower.rs
@@ -0,0 +1,42 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(UserFollower::Table)
+                    .if_not_exists()
+                    .col(ColumnDef::new(UserFollower::Id).uuid().not_null().primary_key())
+                    .col(ColumnDef::new(UserFollower::UserID).string().not_null())
+                    .col(ColumnDef::new(UserFollower::FollowerID).string().not_null())
+                    .to_owned()
+            )
+            .await?;
+
+        Ok(())
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(UserFollower::Table).to_owned())
+            .await?;
+
+        Ok(())
+    }
+}
+
+#[derive(Iden)]
+enum UserFollower {
+    Table,
+    // UUID
+    Id,
+    // 被关注者 ID
+    UserID,
+    // 关注者 ID
+    FollowerID,
+}
