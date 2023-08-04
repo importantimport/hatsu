d3172f00baf1825d46e5b58c974a09dcd04913b0
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-08-05 00:07:32 +0800

    add todo

--- src/protocol/activities/mod.rs
+++ src/protocol/activities/mod.rs
@@ -3,6 +3,10 @@ pub mod create_post;
 mod create_or_update;
 pub use create_or_update::{CreateOrUpdateType, CreateOrUpdateNote};
 
+// TODO:
+// mod deletion;
+// pub use deletion::{DeleteUser, Delete, UndoDelete};
+
 mod following;
 pub use following::{AcceptFollow, Follow, UndoFollow};
 
