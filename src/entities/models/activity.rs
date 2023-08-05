896f408cc343f2f8f2622352ab70832f0f237edb
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-08-05 21:23:34 +0800

    update entity models

--- src/entities/models/activity.rs
+++ src/entities/models/activity.rs
@@ -3,13 +3,15 @@
 use sea_orm::entity::prelude::*;
 
 #[derive(Clone, Debug, PartialEq, DeriveEntityModel, Eq)]
-#[sea_orm(table_name = "post")]
+#[sea_orm(table_name = "activity")]
 pub struct Model {
     #[sea_orm(primary_key, auto_increment = false)]
     pub id: String,
-    pub creator: String,
-    pub text: String,
-    pub local: bool,
+    pub kind: String,
+    pub actor: String,
+    pub object_id: String,
+    pub to: Option<String>,
+    pub cc: Option<String>,
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
