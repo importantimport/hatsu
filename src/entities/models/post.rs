243a56c79bf95d89ba7c4ccdddd1cd0944006b3d
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-08-22 21:18:46 +0800

    new post entity

--- src/entities/models/post.rs
+++ src/entities/models/post.rs
@@ -7,8 +7,11 @@ use sea_orm::entity::prelude::*;
 pub struct Model {
     #[sea_orm(primary_key, auto_increment = false)]
     pub id: String,
-    pub creator: String,
-    pub text: String,
+    pub object: String,
+    pub attributed_to: String,
+    pub published: String,
+    pub updated: Option<String>,
+    pub last_refreshed_at: String,
     pub local: bool,
 }
 
