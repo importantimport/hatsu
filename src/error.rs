1275a2586d5ace4908bfa9beddf7ad9b93e9ccd8
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-08-26 19:42:19 +0800

    json error

--- src/error.rs
+++ src/error.rs
@@ -1,8 +1,11 @@
+use std::fmt::{Display, Formatter};
+
 use axum::{
+    Json,
     response::{IntoResponse, Response},
     http::StatusCode,
 };
-use std::fmt::{Display, Formatter};
+use serde_json::json;
 
 #[derive(Debug)]
 pub struct AppError(pub(crate) anyhow::Error);
@@ -11,7 +14,7 @@ impl IntoResponse for AppError {
     fn into_response(self) -> Response {
         (
             StatusCode::INTERNAL_SERVER_ERROR,
-            format!("{}", self.0)
+            Json(json!({ "error": self.0.to_string() }))
         )
             .into_response()
     }
