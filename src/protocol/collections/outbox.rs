0f2a80ef6b4aa1b71e30d921478550d6020c5831
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-09-03 00:03:55 +0800

    update outbox

--- src/protocol/collections/outbox.rs
+++ src/protocol/collections/outbox.rs
@@ -4,26 +4,40 @@ use url::Url;
 
 use crate::AppError;
 
+pub fn generate_outbox_page_url(outbox_id: &Url, page: u64) -> Result<Url, AppError> {
+    Ok(Url::parse_with_params(&outbox_id.to_string(), &[("page", page.to_string())])?)
+}
+
 #[derive(Clone, Debug, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Outbox {
     #[serde(rename = "type")]
     kind: OrderedCollectionType,
+    // example: https://hatsu.local/u/example.com/outbox
     id: Url,
-    // ordered_items: WithContext<>
-    ordered_items: Vec<()>,
+    // example: https://hatsu.local/u/example.com/outbox?page=1
+    first: Url,
+    // example: https://hatsu.local/u/example.com/outbox?page=64
+    last: Url,
+
     total_items: u64
 }
 
-/// TODO: items (contents)
-/// 应该只输出当前 Feed 存在的文章，其余视为过期
 impl Outbox {
-    pub fn new(outbox_id: Url) -> Result<Self, AppError> {
+    pub fn new(outbox_id: Url, total_items: u64, total_pages: u64) -> Result<Self, AppError> {
         Ok(Self {
-        kind: OrderedCollectionType::OrderedCollection,
-        id: outbox_id,
-        ordered_items: vec![],
-        total_items: 0,
+            kind: OrderedCollectionType::OrderedCollection,
+            id: outbox_id.clone(),
+            first: generate_outbox_page_url(&outbox_id, 1)?,
+            last: generate_outbox_page_url(
+                &outbox_id,
+                // TODO: 测试效果
+                match total_pages {
+                    page if total_pages > 0 => page + 1,
+                    _ => 1,
+                }
+            )?,
+            total_items,
         })
     }
 }
--- src/routes/users/user_outbox.rs
+++ src/routes/users/user_outbox.rs
@@ -1,28 +1,68 @@
 use activitypub_federation::{
     axum::json::FederationJson,
     config::Data, protocol::context::WithContext,
+    // fetch::object_id::ObjectId,
 };
 use axum::{
     debug_handler,
-    extract::Path,
+    extract::{
+        Path,
+        // Query
+    },
     response::{IntoResponse, Redirect},
 };
+// use sea_orm::*;
+// use serde::Deserialize;
 use url::Url;
 
 use crate::{
     AppData,
     AppError,
+    // entities::{
+    //     prelude::*,
+    //     activity,
+    //     user::Model as DbUser,
+    // },
     protocol::collections::outbox::Outbox,
 };
+
+// #[derive(Deserialize)]
+// pub struct Pagination {
+//     page: Option<u64>,
+// }
+
+// impl Default for Pagination {
+//     fn default() -> Self {
+//         Self { page: None }
+//     }
+// }
 
 #[debug_handler]
 pub async fn handler(
     Path(name): Path<String>,
+    // pagination: Option<Query<Pagination>>,
     data: Data<AppData>,
 ) -> Result<FederationJson<WithContext<Outbox>>, AppError> {
+    // let Query(pagination) = pagination.unwrap_or_default();
+
+    // let user_id: ObjectId<DbUser> = Url::parse(&format!("https://{}/u/{}", data.domain(), &name))?.into();
+    // let user = user_id.dereference_local(&data).await?;
+
+    // let activity_pages = user.find_related(Activity)
+    //     // TODO: order by last_refreshed_at
+    //     .order_by_asc(activity::Column::Id)
+    //     // 20 per page
+    //     .paginate(&data.conn, 20);
+
+    // let total = post_pages.num_items_and_pages().await?;
+
     Ok(FederationJson(WithContext::new_default(
         Outbox::new(
-            Url::parse(&format!("https://{}/u/{}/outbox", data.domain(), name))?
+            Url::parse(&format!("https://{}/u/{}/outbox", data.domain(), name))?,
+            // total.number_of_items,
+            // total.number_of_pages,
+            0,
+            0,
         )?
     )))
 }
