ad097d793023366a02a3dc16484b67a865bf43bd
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-09-05 21:07:02 +0800

    outbox pages

--- src/protocol/collections/outbox.rs
+++ src/protocol/collections/outbox.rs
@@ -1,5 +1,6 @@
-use activitypub_federation::kinds::collection::OrderedCollectionType;
+use activitypub_federation::kinds::collection::{OrderedCollectionType, OrderedCollectionPageType};
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use url::Url;
 
 use crate::AppError;
@@ -23,6 +24,30 @@ pub struct Outbox {
     total_items: u64
 }
 
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct OutboxPage {
+    #[serde(rename = "type")]
+    kind: OrderedCollectionPageType,
+    // example: https://hatsu.local/u/example.com/outbox?page=2
+    id: Url,
+
+    // example: https://hatsu.local/u/example.com/outbox?page=1
+    #[serde(skip_serializing_if = "Option::is_none")]
+    prev: Option<Url>,
+    // example: https://hatsu.local/u/example.com/outbox?page=3
+    #[serde(skip_serializing_if = "Option::is_none")]
+    next: Option<Url>,
+
+    // example: https://hatsu.local/u/example.com/followers
+    part_of: Url,
+
+    // activities list (20 per page)
+    ordered_items: Vec<Value>,
+    // activities count
+    total_items: u64
+}
+
 impl Outbox {
     pub fn new(outbox_id: Url, total_items: u64, total_pages: u64) -> Result<Self, AppError> {
         Ok(Self {
@@ -37,6 +62,28 @@ impl Outbox {
                     _ => 1,
                 }
             )?,
+            total_items,
+        })
+    }
+}
+
+impl OutboxPage {
+    pub fn new(outbox_id: Url, total_items: u64, ordered_items: Vec<Value>, total_pages: u64, page: u64) -> Result<Self, AppError> {
+        Ok(Self {
+            kind: OrderedCollectionPageType::OrderedCollectionPage,
+            id: Url::parse_with_params(outbox_id.as_ref(), &[("page", page.to_string())])?,
+            /// 如果当前页数大于 1，则提供上一页
+            prev: match page {
+                page if page > 1 => Some(generate_outbox_page_url(&outbox_id, page - 1)?),
+                _ => None,
+            },
+            /// 如果当前页数小于总页数，则提供下一页
+            next: match page {
+                page if page < total_pages => Some(generate_outbox_page_url(&outbox_id, page + 1)?),
+                _ => None,
+            },
+            part_of: outbox_id,
+            ordered_items,
             total_items,
         })
     }
--- src/routes/users/user_outbox.rs
+++ src/routes/users/user_outbox.rs
@@ -1,70 +1,92 @@
 use activitypub_federation::{
     axum::json::FederationJson,
     config::Data, protocol::context::WithContext,
-    // fetch::object_id::ObjectId,
+    fetch::object_id::ObjectId,
 };
 use axum::{
     debug_handler,
     extract::{
         Path,
-        // Query
+        Query
     },
     response::{IntoResponse, Redirect},
 };
-// use sea_orm::*;
-// use serde::Deserialize;
+use sea_orm::*;
+use serde::Deserialize;
+use serde_json::Value;
 use url::Url;
 
 use crate::{
     AppData,
     AppError,
-    // entities::{
-    //     prelude::*,
-    //     activity,
-    //     user::Model as DbUser,
-    // },
-    protocol::collections::outbox::Outbox,
+    entities::{
+        prelude::*,
+        activity,
+        user::Model as DbUser,
+    },
+    protocol::collections::outbox::{Outbox, OutboxPage},
 };
 
-// #[derive(Deserialize)]
-// pub struct Pagination {
-//     page: Option<u64>,
-// }
-
-// impl Default for Pagination {
-//     fn default() -> Self {
-//         Self { page: None }
-//     }
-// }
+#[derive(Default, Deserialize)]
+pub struct Pagination {
+    page: Option<u64>,
+}
 
 #[debug_handler]
 pub async fn handler(
     Path(name): Path<String>,
-    // pagination: Option<Query<Pagination>>,
+    pagination: Option<Query<Pagination>>,
     data: Data<AppData>,
-) -> Result<FederationJson<WithContext<Outbox>>, AppError> {
-    // let Query(pagination) = pagination.unwrap_or_default();
+) -> Result<FederationJson<WithContext<Value>>, AppError> {
+    let Query(pagination) = pagination.unwrap_or_default();
 
-    // let user_id: ObjectId<DbUser> = Url::parse(&format!("https://{}/u/{}", data.domain(), &name))?.into();
-    // let user = user_id.dereference_local(&data).await?;
+    let user_id: ObjectId<DbUser> = Url::parse(&format!("https://{}/u/{}", data.domain(), &name))?.into();
+    let user = user_id.dereference_local(&data).await?;
 
-    // let activity_pages = user.find_related(Activity)
-    //     // TODO: order by last_refreshed_at
-    //     .order_by_asc(activity::Column::Id)
-    //     // 20 per page
-    //     .paginate(&data.conn, 20);
+    let activity_pages = user.find_related(Activity)
+        .filter(activity::Column::Kind.eq("Create"))
+        // TODO: order by last_refreshed_at
+        .order_by_desc(activity::Column::Published)
+        // .order_by_asc(activity::Column::Id)
+        // 20 per page
+        .paginate(&data.conn, 20);
 
-    // let total = post_pages.num_items_and_pages().await?;
+    let total = activity_pages.num_items_and_pages().await?;
 
-    Ok(FederationJson(WithContext::new_default(
-        Outbox::new(
-            Url::parse(&format!("https://{}/u/{}/outbox", data.domain(), name))?,
-            // total.number_of_items,
-            // total.number_of_pages,
-            0,
-            0,
-        )?
-    )))
+    match pagination.page {
+        None => {
+            Ok(FederationJson(WithContext::new_default(
+                serde_json::to_value(Outbox::new(
+                    Url::parse(&format!("https://{}/u/{}/outbox", data.domain(), name))?,
+                    total.number_of_items,
+                    total.number_of_pages,
+                )?)?
+            )))
+        },
+        Some(page) => {
+            if page > 1 && page > total.number_of_pages {
+                Err(AppError::NotFound {
+                    kind: format!("user {}", name),
+                    name: format!("outbox page {}", page)
+                })
+            } else {
+                Ok(FederationJson(WithContext::new_default(
+                    serde_json::to_value(OutboxPage::new(
+                        Url::parse(&format!("https://{}/u/{}/outbox", data.domain(), name))?,
+                        total.number_of_items,
+                        activity_pages
+                            .fetch_page(page - 1)
+                            .await?
+                            .into_iter()
+                            .map(|activity| activity.into_json().unwrap())
+                            .collect(),
+                        total.number_of_pages,
+                        page
+                    )?)?
+                )))
+            }
+        }
+    }
 }
 
 #[debug_handler]
