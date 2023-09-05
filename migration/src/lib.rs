56727434f93bf4678cd46659e790bedfacca7845
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-09-05 14:38:02 +0800

    rename

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,6 +1,6 @@
 pub use sea_orm_migration::prelude::*;
 
-mod m20220101_000001_create_table;
+mod m20220101_000001_user;
 mod m20230808_000001_activity;
 mod m20230822_000001_post;
 mod m20230831_000001_user_feed_item;
@@ -12,7 +12,7 @@ pub struct Migrator;
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
-            Box::new(m20220101_000001_create_table::Migration),
+            Box::new(m20220101_000001_user::Migration),
             Box::new(m20230808_000001_activity::Migration),
             Box::new(m20230822_000001_post::Migration),
             Box::new(m20230831_000001_user_feed_item::Migration),
