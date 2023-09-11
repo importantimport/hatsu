2e04ae1da66de9b1805714f1ff1e3906af9e273f
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-09-11 12:45:25 +0800

    update comment

--- src/utilities/markdown.rs
+++ src/utilities/markdown.rs
@@ -1,3 +1,5 @@
+/// GitHub Flavored Markdown to HTML
+/// this function never errors with normal markdown because markdown does not have syntax errors.
 pub fn markdown_to_html(value: &str) -> String {
     markdown::to_html_with_options(value, &markdown::Options::gfm()).unwrap()
 }
