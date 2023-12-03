396307a38c609e692c06a39de3e4a589b6135a47
藍+85CD <50108258+kwaa@users.noreply.github.com>
2023-12-03 14:05:56 +0800

    refactor: split crates

--- src/routes/api/mod.rs
+++ src/routes/api/mod.rs
@@ -1,10 +1,9 @@
 use axum::Router;
 
 pub mod v0;
-pub mod v1;
 
 pub fn handler() -> Router {
     Router::new()
         .merge(v0::handler())
-        .merge(v1::handler())
+        .merge(hatsu_api_mastodon::routes::handler())
 }
