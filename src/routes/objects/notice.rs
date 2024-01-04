348bd3bb2bf75f4068d138b971adce8481fc34b6
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-01-04 23:23:17 +0800

    feat: notice redirect

---
+++ src/routes/objects/notice.rs
@@ -0,0 +1,26 @@
+use axum::{
+    debug_handler,
+    extract::Path,
+    response::{Redirect, IntoResponse},
+};
+use hatsu_utils::AppError;
+
+#[debug_handler]
+pub async fn redirect(
+    Path(base64_url): Path<String>,
+) -> impl IntoResponse {
+    let base64 = base64_simd::URL_SAFE;
+
+    match base64.decode_to_vec(&base64_url) {
+        Ok(utf8_url) => {
+            match String::from_utf8(utf8_url) {
+                Ok(url) if url.starts_with("https://") => {
+                    Ok(Redirect::permanent(&format!("/o/{}", url)).into_response())
+                },
+                _ => Err(AppError::not_found("Record", &base64_url))
+            }
+        },
+        _ => Err(AppError::not_found("Record", &base64_url))
+    }
+    // Redirect::permanent(&format!("/o/{}", object)).into_response()
+}
