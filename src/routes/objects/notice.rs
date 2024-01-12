b4267b8afbbd79c5217f158678e1ccbffa02e3fb
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-01-13 00:30:11 +0800

    refactor: use map_or_else

--- src/routes/objects/notice.rs
+++ src/routes/objects/notice.rs
@@ -9,14 +9,13 @@ use hatsu_utils::AppError;
 pub async fn redirect(Path(base64_url): Path<String>) -> impl IntoResponse {
     let base64 = base64_simd::URL_SAFE;
 
-    match base64.decode_to_vec(&base64_url) {
-        Ok(utf8_url) => match String::from_utf8(utf8_url) {
+    base64.decode_to_vec(&base64_url).map_or_else(
+        |_| Err(AppError::not_found("Record", &base64_url)),
+        |utf8_url| match String::from_utf8(utf8_url) {
             Ok(url) if url.starts_with("https://") => {
                 Ok(Redirect::permanent(&format!("/o/{url}")).into_response())
             }
             _ => Err(AppError::not_found("Record", &base64_url)),
         },
-        _ => Err(AppError::not_found("Record", &base64_url)),
-    }
-    // Redirect::permanent(&format!("/o/{}", object)).into_response()
+    )
 }
