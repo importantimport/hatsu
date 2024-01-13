b896eb119f606b5929d5c4bbd49754b047037692
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-01-13 19:11:33 +0800

    refactor(utils): use match

--- crates/utils/src/markdown.rs
+++ crates/utils/src/markdown.rs
@@ -2,5 +2,7 @@
 /// this function never errors with normal markdown because markdown does not have syntax errors.
 #[must_use]
 pub fn markdown_to_html(value: &str) -> String {
-    markdown::to_html_with_options(value, &markdown::Options::gfm()).unwrap_or_else(|_| "".to_string())
+    match markdown::to_html_with_options(value, &markdown::Options::gfm()) {
+        Ok(result) | Err(result) => result
+    }
 }
