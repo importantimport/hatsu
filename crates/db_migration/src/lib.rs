5fac1f0c2f46c09ce40a3b121979b9e2bd516ce8
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-01-28 18:57:44 +0800

    refactor(migration): update filename

--- crates/db_migration/src/lib.rs
+++ crates/db_migration/src/lib.rs
@@ -1,10 +1,10 @@
 pub use sea_orm_migration::prelude::*;
 
-mod m20220101_000001_user;
-mod m20230808_000001_activity;
-mod m20230822_000001_post;
-mod m20230831_000001_user_feed_item;
-mod m20230902_000001_received_follow;
+mod m20240131_000001_user;
+mod m20240131_000002_user_feed_item;
+mod m20240131_000003_post;
+mod m20240131_000004_activity;
+mod m20240131_000005_received_follow;
 
 pub struct Migrator;
 
@@ -12,11 +12,11 @@ pub struct Migrator;
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
-            Box::new(m20220101_000001_user::Migration),
-            Box::new(m20230808_000001_activity::Migration),
-            Box::new(m20230822_000001_post::Migration),
-            Box::new(m20230831_000001_user_feed_item::Migration),
-            Box::new(m20230902_000001_received_follow::Migration),
+            Box::new(m20240131_000001_user::Migration),
+            Box::new(m20240131_000002_user_feed_item::Migration),
+            Box::new(m20240131_000003_post::Migration),
+            Box::new(m20240131_000004_activity::Migration),
+            Box::new(m20240131_000005_received_follow::Migration),
         ]
     }
 }
