f06993ed038fe5171ee5449a8e63173d41a00be2
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-03-05 17:24:04 +0800

    feat: setup devenv

---
+++ flake.nix
@@ -0,0 +1,68 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    devenv.url = "github:cachix/devenv";
+    nix2container.url = "github:nlewo/nix2container";
+    nix2container.inputs.nixpkgs.follows = "nixpkgs";
+    mk-shell-bin.url = "github:rrbutani/nix-mk-shell-bin";
+
+    fenix.url = "github:nix-community/fenix/monthly";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devenv.flakeModule
+      ];
+      # systems = [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      systems = [ "x86_64-linux" ];
+
+      perSystem = { config, self', inputs', pkgs, system, lib, ... }: {
+        # Per-system attributes can be defined here. The self' and inputs'
+        # module parameters provide easy access to attributes of the same
+        # system.
+
+        # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
+        # packages.default = pkgs.hello;
+
+        devenv.shells.default = let
+          rust_toolchain = lib.importTOML ./rust-toolchain.toml;
+        in {
+          name = "hatsu";
+
+          imports = [
+            # This is just like the imports in devenv.nix.
+            # See https://devenv.sh/guides/using-with-flake-parts/#import-a-devenv-module
+            # ./devenv-foo.nix
+          ];
+
+          # https://devenv.sh/reference/options/
+          packages = with pkgs; [
+            just
+          ];
+
+          languages.rust.enable = true;
+          languages.rust.channel = rust_toolchain.toolchain.channel;
+          languages.rust.components = rust_toolchain.toolchain.components;
+
+          enterShell = ''
+            hello
+          '';
+        };
+
+      };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+
+      };
+    };
+}
