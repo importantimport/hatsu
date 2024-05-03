fc6f20116a38f958bb0f27931a7528e84cb2a754
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-05-03 09:05:05 +0800

    chore(api_mastodon): update openapi

--- crates/api_mastodon/src/routes/statuses/status_reblogged_by.rs
+++ crates/api_mastodon/src/routes/statuses/status_reblogged_by.rs
@@ -6,9 +6,6 @@ use sea_orm::{EntityTrait, ModelTrait};
 
 use crate::entities::Account;
 
-// (status = NOT_FOUND, description = "Status does not exist or is private", body = AppError)
-// { "error": "Record not found" }
-
 /// See who boosted a status
 ///
 /// <https://docs.joinmastodon.org/methods/statuses/#reblogged_by>
@@ -18,6 +15,7 @@ use crate::entities::Account;
     path = "/api/v1/statuses/{id}/reblogged_by",
     responses(
         (status = OK, description = "A list of accounts that boosted the status", body = Vec<Account>),
+        (status = NOT_FOUND, description = "Status does not exist or is private", body = AppError),
     ),
     params(
         ("id" = String, Path, description = "The ID of the Status in the database.")
