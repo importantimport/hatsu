693638fa57c49f413e53bbe06d66531adc2d56fd
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-05-15 18:41:09 +0800

    feat(scheduler): update `user_feed`

--- crates/scheduler/src/update/job.rs
+++ crates/scheduler/src/update/job.rs
@@ -6,9 +6,17 @@ use hatsu_db_schema::{
     prelude::User,
     user::{self, Model as DbUser},
 };
-use hatsu_feed::{UserFeedTopLevel, WrappedUserFeedItem};
+use hatsu_feed::{UserFeed, UserFeedTopLevel, WrappedUserFeedItem};
 use hatsu_utils::{AppData, AppError};
-use sea_orm::{ColumnTrait, EntityTrait, QueryFilter, QueryOrder};
+use sea_orm::{
+    ActiveModelTrait,
+    ColumnTrait,
+    EntityTrait,
+    IntoActiveModel,
+    QueryFilter,
+    QueryOrder,
+    Set,
+};
 
 use crate::update::check_feed_item;
 
@@ -25,9 +33,9 @@ pub async fn fast_update(data: &Data<AppData>) -> Result<(), AppError> {
     Ok(())
 }
 
-pub async fn fast_update_per_user(data: &Data<AppData>, user: DbUser) -> Result<(), AppError> {
-    let feed = UserFeedTopLevel::get(user.clone()).await?;
-    let user: ApubUser = user.into();
+pub async fn fast_update_per_user(data: &Data<AppData>, db_user: DbUser) -> Result<(), AppError> {
+    let feed = UserFeedTopLevel::get(db_user.clone()).await?;
+    let user: ApubUser = db_user.into();
 
     for item in feed.items {
         check_feed_item(
@@ -56,14 +64,29 @@ pub async fn full_update(data: &Data<AppData>) -> Result<(), AppError> {
     Ok(())
 }
 
-pub async fn full_update_per_user(data: &Data<AppData>, user: DbUser) -> Result<(), AppError> {
-    let feed = UserFeedTopLevel::get(user.clone())
+pub async fn full_update_per_user(data: &Data<AppData>, db_user: DbUser) -> Result<(), AppError> {
+    let user_feed = UserFeed::get(db_user.preferred_username.to_string()).await?;
+
+    let db_user_feed = Some(user_feed.clone().into_db());
+
+    if !db_user.feed.eq(&db_user_feed) {
+        hatsu_db_schema::user::ActiveModel {
+            feed: Set(db_user_feed),
+            ..db_user.clone().into_active_model()
+        }
+        .update(&data.conn)
+        .await?;
+    }
+
+    let user_feed_top_level = user_feed
+        .get_top_level(&db_user.preferred_username)
         .await?
         .get_full()
         .await?;
-    let user: ApubUser = user.into();
 
-    for item in feed.items {
+    let user: ApubUser = db_user.into();
+
+    for item in user_feed_top_level.items {
         check_feed_item(
             data,
             &user,
