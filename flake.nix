43b490d97fcceade983bf0825250857f61d5848b
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-07-17 00:05:49 +0800

    refactor(flake): use `rust-overlay` instead of `fenix`

--- flake.nix
+++ flake.nix
@@ -9,30 +9,36 @@
     crane.url = "github:ipetkov/crane";
     crane.inputs.nixpkgs.follows = "nixpkgs";
 
-    fenix.url = "github:nix-community/fenix/monthly";
-    fenix.inputs.nixpkgs.follows = "nixpkgs";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ crane, fenix, flake-parts, ... }:
+  outputs = inputs@{ crane, flake-parts, nixpkgs, rust-overlay, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [ ];
       systems = [ "x86_64-linux" "aarch64-linux" ];
 
       perSystem = { config, self', inputs', lib, pkgs, system, ... }:
         let
-          toolchain = with fenix.packages.${system}; combine [
-            (fromToolchainFile {
-              file = ./rust-toolchain.toml;
-              sha256 = "WXllA4dqR/yihB3daUxS89PEF6UNwXH1xd3hETeioZ0=";
-            })
-            # minimal.toolchain
-            # targets.aarch64-unknown-linux-gnu.latest.rust-std
-            # targets.aarch64-unknown-linux-musl.latest.rust-std
-            # targets.x86_64-unknown-linux-gnu.latest.rust-std
-            # targets.x86_64-unknown-linux-musl.latest.rust-std
-          ];
+          crossSystem =
+            if system == "x86_64-linux" then
+              "aarch64-linux"
+            else "x86_64-linux";
+          localSystem = system;
+          overlays = [ rust-overlay.overlays.rust-overlay ];
+          pkgs = import nixpkgs { inherit crossSystem localSystem overlays; };
 
-          craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
+          rustToolchain = (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml).override {
+            # extensions = [ "rust-src" "rust-analyzer" ];
+            targets = [
+              "aarch64-unknown-linux-gnu"
+              "aarch64-unknown-linux-musl"
+              "x86_64-unknown-linux-gnu"
+              "x86_64-unknown-linux-musl"
+            ];
+          };
+
+          craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
 
           # https://github.com/ipetkov/crane/blob/master/docs/source-filtering.md
           src = lib.cleanSourceWith {
@@ -45,36 +51,50 @@
               || (craneLib.filterCargoSources path type);
           };
 
-          commonArgs = {
+          craneArgs = {
             inherit src;
             strictDeps = true;
           };
 
-          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+          cargoArtifacts = craneLib.buildDepsOnly craneArgs;
 
-          crateClippy = craneLib.cargoClippy (commonArgs // {
+          cargoFmt = craneLib.cargoFmt {
+            inherit src;
+          };
+
+          cargoClippy = craneLib.cargoClippy (craneArgs // {
             inherit cargoArtifacts;
             cargoClippyExtraArgs = "--all-targets -- -W clippy::pedantic -W clippy::nursery -A clippy::missing-errors-doc -A clippy::module_name_repetitions";
           });
 
-          crateFmt = craneLib.cargoFmt {
-            inherit src;
-          };
-
-          crate = craneLib.buildPackage (commonArgs // {
+          hatsu = craneLib.buildPackage (craneArgs // {
             inherit cargoArtifacts;
           });
 
+          # TODO: cross test
+          # hatsu-aarch64-unknown-linux-gnu = craneLib.buildPackage
+          #   (craneArgs // {
+          #     CARGO_BUILD_TARGET = "aarch64-unknown-linux-gnu";
+          #     CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER = "${pkgs.stdenv.cc.targetPrefix}cc";
+          #     CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_RUNNER = "qemu-aarch64";
+
+          #     HOST_CC = "${pkgs.stdenv.cc.nativePrefix}cc";
+          #     TARGET_CC = "${pkgs.stdenv.cc.targetPrefix}cc";
+          #   });
         in
         {
           checks = {
-            inherit crate crateClippy crateFmt;
+            inherit hatsu cargoFmt cargoClippy;
           };
 
-          packages.default = crate;
+          packages = {
+            default = hatsu;
+            # TODO: cross test
+            # aarch64-unknown-linux-gnu = hatsu-aarch64-unknown-linux-gnu;
+          };
           devShells.default = craneLib.devShell {
             # checks = self'.checks.${system};
-            inputsFrom = [ crate ];
+            inputsFrom = [ hatsu ];
             packages = with pkgs; [
               mdbook # ./docs/
 
