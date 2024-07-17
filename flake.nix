08d92e1415aa2f53e5837410f581acd1bf642a3a
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-07-17 12:18:25 +0800

    fix(flake): select latest nightly rust

--- flake.nix
+++ flake.nix
@@ -28,15 +28,18 @@
           overlays = [ rust-overlay.overlays.rust-overlay ];
           pkgs = import nixpkgs { inherit crossSystem localSystem overlays; };
 
-          rustToolchain = (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml).override {
-            # extensions = [ "rust-src" "rust-analyzer" ];
+          rustToolchain = pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+            extensions = [
+              "rust-src"
+              # "rust-analyzer"
+            ];
             targets = [
               "aarch64-unknown-linux-gnu"
               "aarch64-unknown-linux-musl"
               "x86_64-unknown-linux-gnu"
               "x86_64-unknown-linux-musl"
             ];
-          };
+          });
 
           craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
 
