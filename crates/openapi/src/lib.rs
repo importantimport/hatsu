b731780a4d1a8ba561d95d3474fe791927e9513e
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-09-14 14:09:34 +0800

    feat(openapi): scalar frontend

--- crates/openapi/src/lib.rs
+++ crates/openapi/src/lib.rs
@@ -1,14 +1,15 @@
 use axum::{routing::get, Json, Router};
 use utoipa::OpenApi;
-// use utoipa_swagger_ui::SwaggerUi;
+use utoipa_scalar::{Scalar, Servable};
 
 mod apidoc;
 pub use apidoc::{ApiDoc, SecurityAddon};
 
 pub fn routes() -> Router {
-    Router::new().route(
-        "/openapi.json",
-        get(|| async move { Json(ApiDoc::openapi()) }),
-    )
-    // .merge(SwaggerUi::new("/swagger-ui").url("/openapi.json", ApiDoc::openapi()))
+    Router::new()
+        .route(
+            "/openapi.json",
+            get(|| async move { Json(ApiDoc::openapi()) }),
+        )
+        .merge(Scalar::with_url("/scalar", ApiDoc::openapi()))
 }
