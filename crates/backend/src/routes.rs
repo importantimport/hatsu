cdfce9bbc432d07ef91c928d2a9a946e2e984c4b
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-09-14 15:14:39 +0800

    refactor: move openapi routes to backend

--- crates/backend/src/routes.rs
+++ crates/backend/src/routes.rs
@@ -1,7 +1,8 @@
-use axum::{http::Response, routing::get, Router};
+use axum::{http::Response, routing::get, Json, Router};
 use hatsu_utils::AppEnv;
 use utoipa::OpenApi;
 use utoipa_axum::router::OpenApiRouter;
+use utoipa_scalar::{Scalar, Servable};
 
 use crate::favicon;
 
@@ -11,16 +12,21 @@ async fn root() -> Response<String> {
 }
 
 pub fn routes() -> Router {
-    let (api_router, _api) = OpenApiRouter::with_openapi(hatsu_openapi::ApiDoc::openapi())
+    let (api_router, api) = OpenApiRouter::with_openapi(hatsu_openapi::ApiDoc::openapi())
         .merge(hatsu_api::routes())
         .split_for_parts();
+
+    let openapi_json = api.clone();
+
+    let api_router = api_router
+        .route("/openapi.json", get(|| async move { Json(openapi_json) }))
+        .merge(Scalar::with_url("/scalar", api));
 
     let router = Router::new()
         .merge(hatsu_api_admin::routes())
         .merge(hatsu_api_apub::routes())
         .merge(hatsu_api_mastodon::routes())
         .merge(hatsu_nodeinfo::routes())
-        .merge(hatsu_openapi::routes())
         .merge(hatsu_well_known::routes())
         .route("/", get(root))
         .route("/favicon.ico", get(favicon::ico))
--- crates/openapi/src/lib.rs
+++ crates/openapi/src/lib.rs
@@ -1,15 +1,2 @@
-use axum::{routing::get, Json, Router};
-use utoipa::OpenApi;
-use utoipa_scalar::{Scalar, Servable};
-
 mod apidoc;
 pub use apidoc::{ApiDoc, SecurityAddon};
-
-pub fn routes() -> Router {
-    Router::new()
-        .route(
-            "/openapi.json",
-            get(|| async move { Json(ApiDoc::openapi()) }),
-        )
-        .merge(Scalar::with_url("/scalar", ApiDoc::openapi()))
-}
