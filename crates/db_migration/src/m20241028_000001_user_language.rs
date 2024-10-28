2d95cc45f08c6db31c6e0871fb59053a4310cf58
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-10-28 16:08:27 +0800

    feat(migration): add user.language

---
+++ crates/db_migration/src/m20241028_000001_user_language.rs
@@ -0,0 +1,35 @@
+use sea_orm_migration::{prelude::*, schema::*};
+
+use crate::m20240131_000001_user::User;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(User::Table)
+                    .add_column(string_null(User::Language))
+                    .to_owned(),
+            )
+            .await?;
+
+        Ok(())
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(User::Table)
+                    .drop_column(User::Language)
+                    .to_owned(),
+            )
+            .await?;
+
+        Ok(())
+    }
+}
