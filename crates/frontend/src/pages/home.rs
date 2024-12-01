0ccb6e1a6d872c33d56a0a43c02f01725667082d
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-12-01 18:27:37 +0800

    feat(frontend): home page

--- crates/frontend/src/pages/home.rs
+++ crates/frontend/src/pages/home.rs
@@ -1,10 +1,38 @@
-use hatsu_utils::AppEnv;
+use activitypub_federation::config::Data;
+use hatsu_utils::AppData;
 use maud::{html, Markup};
 
 use crate::partials::layout;
 
-pub async fn home() -> Markup {
-    layout(html! {
-        h1 class="md-typescale-display-medium" { (AppEnv::info()) }
-    })
+pub async fn home(data: Data<AppData>) -> Markup {
+    let title = data
+        .env
+        .hatsu_node_name
+        .clone()
+        .unwrap_or(String::from("Hatsu"));
+
+    layout(
+        html! {
+            @if let Some(description) = &data.env.hatsu_node_description {
+                h2 class="md-typescale-title-large" style="margin-top: 0" { "About this instance" }
+                p style="margin: 0" { (description) }
+                br;
+                md-divider {}
+            }
+            h2 class="md-typescale-title-large" { "What is this?" }
+            p style="margin: 0" { r#"
+                The web page you're reading right now is served by an instance of Hatsu,
+                a self-hosted bridge that interacts with Fediverse on behalf of static site.
+            "# }
+            br;
+            md-divider {}
+            h2 class="md-typescale-title-large" { "Register an Account on " (title) }
+            p style="margin: 0" {
+                "New account registration is currently "
+                b { "closed" }
+                "."
+            }
+        },
+        &data,
+    )
 }
--- crates/frontend/src/partials/layout.rs
+++ crates/frontend/src/partials/layout.rs
@@ -1,10 +1,11 @@
+use activitypub_federation::config::Data;
+use hatsu_utils::{AppData, AppEnv};
 use maud::{html, Markup, PreEscaped, DOCTYPE};
 use serde_json::json;
 
 static CSS_CUSTOM: &str = include_str!("../../assets/custom.css");
 static CSS_DARK: &str = include_str!("../../assets/dark.css");
 static CSS_LIGHT: &str = include_str!("../../assets/light.css");
-static CSS_TYPESCALE: &str = include_str!("../../assets/typescale.css");
 
 fn material_web() -> Markup {
     html! {
@@ -13,8 +14,6 @@ fn material_web() -> Markup {
             (CSS_DARK)
             "\n"
             (CSS_LIGHT)
-            "\n"
-            (CSS_TYPESCALE)
             "\n"
             (CSS_CUSTOM)
         }
@@ -27,7 +26,10 @@ fn material_web() -> Markup {
         }
         script type="module" {
             r#"
-import '@material/web/all.js';
+import '@material/web/divider/divider.js';
+
+import '@material/web/labs/card/filled-card.js';
+
 import { styles as typescaleStyles } from '@material/web/typography/md-typescale-styles.js';
 document.adoptedStyleSheets.push(typescaleStyles.styleSheet);
             "#
@@ -35,7 +37,13 @@ document.adoptedStyleSheets.push(typescaleStyles.styleSheet);
     }
 }
 
-pub fn layout(body: Markup) -> Markup {
+pub fn layout(body: Markup, data: &Data<AppData>) -> Markup {
+    let title = data
+        .env
+        .hatsu_node_name
+        .clone()
+        .unwrap_or(String::from("Hatsu"));
+
     html! {
         (DOCTYPE)
         html lang="en" {
@@ -44,11 +52,26 @@ pub fn layout(body: Markup) -> Markup {
                 meta name="viewport" content="width=device-width, initial-scale=1.0";
                 link rel="icon" href="/favicon.ico" sizes="48x48";
                 link rel="icon" href="/favicon.svg" sizes="any" type="image/svg+xml";
-                title { "Hatsu" }
+                title { (&title) }
                 (material_web())
             }
-            body {
-                (body)
+            body class="md-typescale-body-large" {
+                header {
+                    h2 class="md-typescale-title-large" style="margin: 1rem" { (title) }
+                }
+                main {
+                    md-filled-card style="padding: 1rem" {
+                        (body)
+                    }
+                }
+                footer {
+                    div style="padding: 1rem; display: flex; gap: 0.5rem" {
+                        img src="favicon.svg" height="24" width="24";
+                        a href="https://github.com/importantimport/hatsu" target="_blank" rel="noopener"  {
+                            (AppEnv::info())
+                        }
+                    }
+                }
             }
         }
     }
